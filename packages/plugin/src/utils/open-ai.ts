c466c53d826581ab8efd8448331ed3568b434e25
John Dunning <fw@johndunning.com>
2023-06-20 19:05:36 -0700

    Call OpenAI to generate default error messages
    Add dropdown.ts to output a select menu in Form.io.
    Open the heroku preview page to show the newly exported form when plugin is done.
    Show key values before and after calling OpenAI.
    Add dev:plugin command.

--- packages/plugin/src/utils/open-ai.ts
+++ packages/plugin/src/utils/open-ai.ts
@@ -1,13 +1,29 @@
 import { FormioJSON } from "@/types";
 
 //const GenerateKeysURL = "http://127.0.0.1:3000/api/generateKeys";
-const GenerateKeysURL = "https://faas-sfo3-7872a1dd.doserverless.co/api/v1/web/fn-ebdb2c50-b3cd-475a-a51d-2cf90d5b6185/openai/generateKeys";
+//const GenerateKeysURL = "https://faas-sfo3-7872a1dd.doserverless.co/api/v1/web/fn-ebdb2c50-b3cd-475a-a51d-2cf90d5b6185/openai/generateKeys";
+const BaseURL = "https://faas-sfo3-7872a1dd.doserverless.co/api/v1/web/fn-ebdb2c50-b3cd-475a-a51d-2cf90d5b6185/openai";
 
 export async function generateKeys(
 	panels: FormioJSON[]): Promise<FormioJSON[]>
 {
 	const body = JSON.stringify({ panels });
-	const response = await fetch(GenerateKeysURL, {
+	const response = await fetch(`${BaseURL}/generateKeys`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body
+	});
+
+	return (await response.json()).result;
+}
+
+export async function generateErrors(
+	panels: FormioJSON[]): Promise<FormioJSON[]>
+{
+	const body = JSON.stringify({ panels });
+	const response = await fetch(`${BaseURL}/generateErrors`, {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json"
